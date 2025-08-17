6e3cc78c2875cc9b8586f871485f3228c706ec4b
Esther Freixo <freixo.esther@gmail.com>
2025-08-17 20:47:20 -0300

    add: auth

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -24,6 +24,7 @@ app.setErrorHandler((error, resquest, reply) => {
     if (error instanceof ZodError) {
       return reply.status(400).send({ message: 'Validation error', issues: error.format() })
     }
+    
   
     return reply.status(500).send({ message: "Internal server error" })
   })
