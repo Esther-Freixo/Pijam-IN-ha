ea46fc222d496a951a0a70582c317b03b133ab21
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-18 23:03:00 -0300

    feat: hook criado para a avaliação com estrelas

---
+++ apps/frontend/front-pijaminha/src/hooks/useEstrela.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export const useEstrela = () => {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    const handleClick = (value: number) => {
+        setRating(value);
+    };
+
+    const handleMouseEnter = (value: number) => {
+        setHover(value);
+    };
+
+    const handleMouseLeave = () => {
+        setHover(0);
+    };
+
+    return {
+        rating,
+        hover,
+        handleClick,
+        handleMouseEnter,
+        handleMouseLeave
+    };
+};
