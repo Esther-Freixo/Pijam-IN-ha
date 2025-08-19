d22ca5703fc061b2865ea0121b5c3a0b8ba2eed0
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-18 23:54:57 -0300

    feat: estrutura e estilização do registro

--- apps/frontend/front-pijaminha/src/Pages/Cadastro/index.tsx
+++ apps/frontend/front-pijaminha/src/Pages/Cadastro/index.tsx
@@ -3,7 +3,34 @@ import styles from "./styles.module.css"
 export default function Cadastro() {
     return (
         <>
-            
+            <div className={styles.main}>
+                <div className={styles.box}>
+                    <h1>Registre-se</h1>
+                    <form className={styles.form}>
+                        <input
+                            type="name"
+                            placeholder='Nome'
+                        />
+                        <input
+                            type="name"
+                            placeholder='Nome de usuário'
+                        />
+                        <input
+                            type="email"
+                            placeholder='E-mail'
+                        />
+                        <input
+                            type="password"
+                            placeholder='Senha'
+                        />
+                        <input
+                            type="password"
+                            placeholder='Confirmar senha'
+                        />
+                        <button className={styles.botaoRegistrar}>Registrar</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
