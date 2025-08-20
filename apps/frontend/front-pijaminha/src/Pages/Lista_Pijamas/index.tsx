91aa3b83910e69bd505cd4bf42f14fdc7a73a3b7
Marcos_V_Costa <130842432+Mavini97@users.noreply.github.com>
2025-08-20 02:50:08 -0300

    Estilização e criação de componentes foram criados para a pagina de pijamas

--- apps/frontend/front-pijaminha/src/Pages/Lista_Pijamas/index.tsx
+++ apps/frontend/front-pijaminha/src/Pages/Lista_Pijamas/index.tsx
@@ -1,26 +1,52 @@
-import React, { useState } from "react";
-import SearchBar from "../../components/Pesquisa";
-//import ProductList from "../../components/ProductList";
+
+import ProductList from "../../components/ProductList";
 import Pagination from "../../components/Pagination";
-//import produtosData from "../../../backend/produtos.json";
+import produtosData from "../../produtos.json";
+import SearchBar from "../../components/Pesquisa";
+import { useState } from "react";
+
+interface Filtros {
+  nome: string;
+  genero: string;
+  tipo: string;
+  estacao: string;
+}
 
 export default function Lista_Pijamas() {
   const [paginaAtual, setPaginaAtual] = useState(1);
+  const [filtrosDeBusca, setFiltrosDeBusca] = useState<Filtros>({
+    nome: '',
+    genero: '',
+    tipo: '',
+    estacao: '',
+  });
   const produtosPorPagina = 12;
+  const produtosFiltrados = produtosData.filter(produto => {
+    return (
+      produto.nome.toLowerCase().includes(filtrosDeBusca.nome.toLowerCase()) &&
+      (filtrosDeBusca.genero === '' || produto.genero === filtrosDeBusca.genero) &&
+      (filtrosDeBusca.tipo === '' || produto.tipo === filtrosDeBusca.tipo) &&
+      (filtrosDeBusca.estacao === '' || produto.estacao === filtrosDeBusca.estacao)
+    );
+  });
 
   const indiceFinal = paginaAtual * produtosPorPagina;
   const indiceInicial = indiceFinal - produtosPorPagina;
-  const produtosDaPagina = produtosData.slice(indiceInicial, indiceFinal);
-
-  const totalPaginas = Math.ceil(produtosData.length / produtosPorPagina);
+  const produtosDaPagina = produtosFiltrados.slice(indiceInicial, indiceFinal);
+  const totalPaginas = Math.ceil(produtosFiltrados.length / produtosPorPagina);
 
   const handleMudarPagina = (numeroDaPagina: number) => {
     setPaginaAtual(numeroDaPagina);
   };
 
+  const handleSearch = (filtros: Filtros) => {
+    setFiltrosDeBusca(filtros);
+    setPaginaAtual(1);
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} />
       <ProductList produtos={produtosDaPagina} />
       <Pagination
         paginaAtual={paginaAtual}
@@ -29,4 +55,4 @@ export default function Lista_Pijamas() {
       />
     </div>
   );
-}
+};
