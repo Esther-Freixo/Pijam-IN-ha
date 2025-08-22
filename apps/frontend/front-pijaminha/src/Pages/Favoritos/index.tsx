231b9b0c23282b30c4b3b083b9c218f71fbbb996
Ana Luiza Cunha <analuizacunha@id.uff.br>
2025-08-22 16:26:51 -0300

    feat: adiciona componente para paginas favoritos e carrinho

---
+++ apps/frontend/front-pijaminha/src/Pages/Favoritos/index.tsx
@@ -0,0 +1,13 @@
+import HeaderCarrinhoEFavoritos from "../../components/HeaderCarrinhoEFavoritos";
+
+export default function Favoritos() {
+  return (
+    <>
+      <HeaderCarrinhoEFavoritos
+        carrinhoColor={"#A4A8B0"}
+        favoriteColor={"#A31621"}
+      />
+      <h1>favoritos</h1>
+    </>
+  );
+}
