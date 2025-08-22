1ba898b70d673edf5572de3703e347a76025aa95
Ana Luiza Cunha <analuizacunha@id.uff.br>
2025-08-22 15:06:05 -0300

    feat: adiciona rotas para requisicao de pijamas e feedbacks

--- apps/frontend/front-pijaminha/src/RootLayout.tsx
+++ apps/frontend/front-pijaminha/src/RootLayout.tsx
@@ -2,19 +2,16 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import { PijamasProvider } from "./stores/pijamasProvider";
-import { FeedbacksProvider } from "./stores/feedbacksProvider";
 
 export default function RootLayout() {
   return (
     <>
       <PijamasProvider>
-        <FeedbacksProvider>
-          <Header />
-          <main>
-            <Outlet />
-          </main>
-          <Footer />
-        </FeedbacksProvider>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
       </PijamasProvider>
     </>
   );
--- apps/frontend/front-pijaminha/src/hooks/usePijamas.tsx
+++ apps/frontend/front-pijaminha/src/hooks/usePijamas.tsx
@@ -5,13 +5,12 @@ import axios from "axios";
 export function usePijamas() {
   const [pijamas, setPijamas] = useState<Pijama[]>([]);
 
-  const url = "http://localhost:3001/produtos";
+  const url = "http://localhost:3333/pijamas";
   useEffect(() => {
     axios
       .get(url)
       .then((response) => {
-        console.log(response);
-        setPijamas(response.data);
+        setPijamas(response.data.pajamas);
       })
       .catch((error) => console.error("Algo deu errado: " + error));
   }, []);
