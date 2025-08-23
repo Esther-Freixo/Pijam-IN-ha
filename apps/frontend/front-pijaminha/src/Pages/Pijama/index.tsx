05ea4a9881199499d95578ac040e11044c9c80bb
Ana Luiza Cunha <analuizacunha@id.uff.br>
2025-08-22 21:21:30 -0300

    feat: ajustes nas paginas favoritos, lista de pijamas e pijama individual

--- apps/frontend/front-pijaminha/src/Pages/Pijama/index.tsx
+++ apps/frontend/front-pijaminha/src/Pages/Pijama/index.tsx
@@ -11,11 +11,18 @@ import { usePijamasContext } from "../../hooks/usePijamasContext";
 import { useParams } from "react-router-dom";
 import { useCart } from "../../stores/carrinhoContext";
 import { useNavigate } from "react-router-dom";
+import verao from "../../assets/icons/verao.png";
+import feminino from "../../assets/icons/feminino.png";
+import ambos from "../../assets/icons/ambos.png";
+import infantil from "../../assets/icons/infantil.png";
+import familia from "../../assets/icons/familia.png";
+import masculino from "../../assets/icons/masculino.png";
 
 export default function Pijama() {
   const { pijamas } = usePijamasContext();
   const { pijamaId } = useParams();
   const [selectedSize, setSelectedSize] = useState<Size | null>(null);
+  const [selectedStock, setSelectedStock] = useState<number | null>(null);
   const pijama = pijamas.find((pijama) => pijama.id === pijamaId);
   const [isFavorited, setIsFavorited] = useState<boolean>(
     () => pijama?.favorite ?? false
@@ -27,10 +34,39 @@ export default function Pijama() {
     return <div>Pijama nao encontrado</div>;
   }
 
-  const priceOnSale: number = pijama.price * (1 - (pijama.sale_percent ?? 0));
+  const handleFavoriteClick = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3333/pajamas/${pijama.id}/favorite`,
+        {
+          method: "PATCH",
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setIsFavorited(data.pajama.favorite);
+        console.log("funcionou");
+      } else {
+        alert("Erro ao atualizar favorito");
+      }
+    } catch (error) {
+      console.error("Erro de conexão com o servidor", error);
+    }
+  };
+
+  const sizes: Size[] = pijama.sizes.map((s) => s.size as Size);
+
+  const handleSizeClick = (size: Size) => {
+    setSelectedSize(size);
+    const sizeObj = pijama.sizes.find((s) => s.size === size);
+    setSelectedStock(sizeObj ? sizeObj.stock_quantity : null);
+  };
+
+  const priceOnSale: number =
+    pijama.price * ((100 - (pijama.sale_percent ?? 0)) / 100);
   const pricePix: number = (pijama.on_sale ? priceOnSale : pijama.price) * 0.85;
   const installmentPrice = (pijama.price / 6).toFixed(2);
-  const sizes: Size[] = ["PP", "P", "M", "G", "GG"];
+
   return (
     <>
       <section className={style.section}>
@@ -82,45 +118,49 @@ export default function Pijama() {
                   key={size}
                   size={size}
                   isSelected={selectedSize === size}
-                  onClick={() => setSelectedSize(size)}
+                  onClick={() => handleSizeClick(size)}
                 />
               ))}
             </div>
             {selectedSize && (
               <p>
-                Ainda temos <span>8</span> peças do tamanho escolhido em nosso
-                estoque!
+                Ainda temos <span>{selectedStock ?? 0}</span> peças do tamanho
+                escolhido em nosso estoque!
               </p>
             )}
           </div>
           <Quantidade
-  quantity={quantity}
-  onIncrease={() => setQuantity((prev) => prev + 1)}
-  onDecrease={() => setQuantity((prev) => Math.max(1, prev - 1))}
-/>
+            quantity={quantity}
+            onIncrease={() => {
+              if (selectedStock !== null && quantity < selectedStock) {
+                setQuantity((prev) => prev + 1);
+              }
+            }}
+            onDecrease={() => setQuantity((prev) => Math.max(1, prev - 1))}
+          />
           <div className={style.cartAndFavoriteSection}>
             <button
-  onClick={() => {
-    if (!selectedSize) {
-      alert("Selecione um tamanho");
-      return;
-    }
+              onClick={() => {
+                if (!selectedSize) {
+                  alert("Selecione um tamanho");
+                  return;
+                }
 
-  const item = {
-  id: String(pijama.id),
-  name: pijama.name,
-  image: pijama.image,
-  price: pijama.on_sale ? priceOnSale : pijama.price,
-  quantity: quantity,
-  size: selectedSize,
-};
+                const item = {
+                  id: String(pijama.id),
+                  name: pijama.name,
+                  image: pijama.image,
+                  price: pijama.on_sale ? priceOnSale : pijama.price,
+                  quantity: quantity,
+                  size: selectedSize,
+                };
 
-addToCart(item);
-navigate("/"); // Vai pra Home
-  }}
->
-  ADICIONAR AO CARRINHO
-</button>
+                addToCart(item);
+                navigate("/");
+              }}
+            >
+              ADICIONAR AO CARRINHO
+            </button>
             <div
               className={`${style.favoriteIcon} ${
                 isFavorited ? style.favorited : ""
@@ -128,7 +168,7 @@ navigate("/"); // Vai pra Home
             >
               <img
                 src={isFavorited ? favoritedIcon : favoriteIcon}
-                onClick={() => setIsFavorited(!isFavorited)}
+                onClick={handleFavoriteClick}
                 alt={
                   isFavorited
                     ? "Remover dos favoritos"
@@ -140,9 +180,19 @@ navigate("/"); // Vai pra Home
         </div>
       </section>
       <div className={style.iconsContainer}>
-        <img src={inverno} alt="" />
-        <img src={unissex} alt="" />
-        <img src={adulto} alt="" />
+        {pijama.season === "verao" && <img src={verao} alt="Verão" />}
+        {pijama.season === "inverno" && <img src={inverno} alt="Inverno" />}
+
+        {pijama.type === "adulto" && <img src={adulto} alt="Adulto" />}
+        {pijama.type === "infantil" && <img src={infantil} alt="Infantil" />}
+        {pijama.type === "familia" && <img src={familia} alt="Família" />}
+
+        {pijama.gender === "feminino" && <img src={feminino} alt="Feminino" />}
+        {pijama.gender === "masculino" && (
+          <img src={masculino} alt="Masculino" />
+        )}
+        {pijama.gender === "unissex" && <img src={unissex} alt="Unissex" />}
+        {pijama.gender === "ambos" && <img src={ambos} alt="Ambos" />}
       </div>
       <div className={style.descriptionSection}>
         <h3>SOBRE NOSSO PIJAMA</h3>
--- apps/frontend/front-pijaminha/src/router.tsx
+++ apps/frontend/front-pijaminha/src/router.tsx
@@ -6,11 +6,8 @@ import Feedback from "./Pages/Feedback";
 import Cadastro from "./Pages/Cadastro";
 import Pijama from "./Pages/Pijama";
 import Lista_Pijamas from "./Pages/Lista_Pijamas";
-<<<<<<< HEAD
-=======
 import Carrinho from "./Pages/Carrinho";
 
->>>>>>> 318cb3d96e6706cc49fb5ab95c6785c099b4c214
 import Favoritos from "./Pages/Favoritos";
 
 const router = createBrowserRouter([
@@ -27,15 +24,8 @@ const router = createBrowserRouter([
       { path: "cadastro", element: <Cadastro /> },
       { path: "pijamas/:pijamaId", element: <Pijama /> },
       { path: "lista_pijamas", element: <Lista_Pijamas /> },
-<<<<<<< HEAD
       { path: "favoritos", element: <Favoritos /> },
-=======
-      { path: "carrinho", element: <Carrinho/> },
-      {
-        path: "favoritos",
-        element: <Favoritos />,
-      },
->>>>>>> 318cb3d96e6706cc49fb5ab95c6785c099b4c214
+      { path: "carrinho", element: <Carrinho /> },
     ],
   },
 ]);
